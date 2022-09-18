0523b88f1d0ff9540eefddc5f04c906a95d45670
jay <xplorerjay@gmail.com>
2022-09-18 19:54:59 +0530

    changes saved

--- all.cpp
+++ all.cpp
@@ -11,5 +11,21 @@ int main(){
     cout<< "hello\n\nbasic 2 line "<<endl;
 }
 
-// Second program
+// Second program (add 2 numbers)
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"\n Program to find sum of 2 numbers\n";
+    cout<<"\n Enter a number: "; cin>>x;
+    cout<< "Enter 2nd number: "; cin>>y;
+    cout<<"Sum is: "<<x+y;
+}
+
+// Third program
+
+
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    const int num = 10;
-    num = 19;
-    cout<< num;
+    int x,y;
+    cout<<"\n Program to find sum of 2 numbers\n";
+    cout<<"\n Enter a number: "; cin>>x;
+    cout<< "Enter 2nd number: "; cin>>y;
+    cout<<"Sum is: "<<x+y;
 }
