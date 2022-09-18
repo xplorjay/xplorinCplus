886637c436ee2568c4dc7908ab9422317eac200b
jay <xplorerjay@gmail.com>
2022-09-18 19:44:22 +0530

    test commit

---
+++ all.cpp
@@ -0,0 +1,15 @@
+/* Logging my c++ learning progress
+with this main file */
+
+// First program
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<< "hello\n\nbasic 2 line "<<endl;
+}
+
+// Second program
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int num = 10;
+    num = 19;
+    cout<< num;
+}
